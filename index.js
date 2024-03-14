63af3541eeffd57f52ad9c39434d66638bd36a45
joesheva <159451033+joesheva@users.noreply.github.com>
2024-03-14 18:19:50 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,54 @@
+
+window.onscroll = function() {scrollFunction()};
+
+
+
+
+
+function scrollFunction() {
+  if (document.body.scrollTop > 105 || document.documentElement.scrollTop > 105) {
+    document.getElementById("header").style.backgroundColor = "";
+    document.getElementById("navbar").style.padding = "20px 10px";
+  
+    document.getElementById("text").style.fontSize = "1rem";
+    document.getElementById("text1").style.fontSize = "1rem";
+    document.getElementById("text2").style.fontSize = "1rem";
+    document.getElementById("text3").style.fontSize = "1rem";
+    document.getElementById("text").style.color = "#0f0f10";
+    document.getElementById("text1").style.color = "#0f0f10";
+    document.getElementById("text2").style.color = "#0f0f10";
+    document.getElementById("text3").style.color = "#0f0f10";
+  } else {
+    document.getElementById("navbar").style.padding = "50px 10px";
+    document.getElementById("text").style.color = "#f6ecde";
+    document.getElementById("text1").style.color = "#f6ecde";
+    document.getElementById("text2").style.color = "#f6ecde";
+    document.getElementById("text3").style.color = "#f6ecde";
+    document.getElementById("text").style.fontSize = "1.7rem";
+    document.getElementById("text1").style.fontSize = "1.7rem";
+    document.getElementById("text2").style.fontSize = "1.7rem";
+    document.getElementById("text3").style.fontSize = "1.7rem";
+    document.getElementById("header").style.backgroundColor = "#0f0f10";
+  }
+}
+
+
+const divs = document.querySelectorAll(".back-chang");
+const toolbar = document.querySelector(".toolbar");
+const backDrop = document.querySelector("#back-drop");
+
+function changeBG(div) {
+  const img = div.target.src;
+  backDrop.style.background = "url('" + img + "')";
+  backDrop.style.backgroundRepeat = "no-repeat";
+  backDrop.style.backgroundSize = "100% 100%"
+  toolbar.classList.add("active")
+}
+
+divs.forEach((i) => {
+  i.addEventListener("mousedown", changeBG)
+});
+
+
+var spanWidth = $('#text span').width();
+$('#text').animate( { width: spanWidth }, 3000 );
