900cbcd44eceb484b3fefc24df3ca627c36133c8
joesheva <159451033+joesheva@users.noreply.github.com>
2024-04-10 14:51:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,71 @@
+//Get elements from the DOM 
+const slider = document.querySelector(".circular-slider");
+const image = document.querySelector(".slider .img");
+const indicator = document.querySelector(".indicator");
+const menuItems = document.querySelectorAll(".menu span");
+const descriptions = document.querySelectorAll(".text");
 
+//Rotation value for each menu item
+const rotationValues = [-65, -40, -20, 0, 20, 40, 65];
+
+
+
+//image names to add for each menu item
+const images = [
+    'taskcopy', 'kitcopy', "reciepecopy", "ordercopy","Untitled-1", "Untitled-5copy", "budgetcopy"
+];
+
+//current menu item index
+let itemIndex = 2;
+
+//function gfor rotating the slider 
+function rotate(rotationValue) {
+    /*rotate the image using the value we 
+    pass when this function is called */
+    image.style.transform = `rotate(${rotationValue}deg)`;
+    /* rotate the indicatorusing the value we pass when this fucntion is called */
+    indicator.style.transform = `translate(-50%, -50%) rotate(${rotationValue}deg)`;
+}
+
+//loop through each meny item
+menuItems.forEach((menuItem, i) => {
+    //add a click event to each menu item
+    menuItem.addEventListener("click", () => {
+        //add the image url to the image for each menu tiem
+        image.style.backgroundImage = "url(images/" + images[i] + ".pdf)";
+        //add the colors for each menu item
+
+        /*create the short counter-rotation
+        before the actual rotation*/
+        /*if the menu item you click has a 
+        greater index value than the previous one */
+        if(i > itemIndex) {
+            //make the counter-rotation counter-clockwise
+            rotate(rotationValues[itemIndex] - 50);
+        } else if (i < itemIndex) {
+            //make the counter-rotation clockwise
+            rotate(rotationValues[itemIndex] + 50);
+        } else {
+            return "";
+        }
+
+        //wait for the counter-rotation to finish 
+        setTimeout(() => {
+            //rotate using the rotationvalues array
+            //assign each array value to the corresspnding menu item
+            rotate(rotationValues[i]);
+        }, 300);
+
+        //hide all descritptions
+        descriptions.forEach(text => {
+            text.style.display = "none";
+        });
+        //show only the corresponding descrption
+        descriptions[i].style.display = "block";
+        //update the itemindex variable to the current menu item index
+        itemIndex = i;
+    });
+});
 window.onscroll = function() {scrollFunction()};
 
 
