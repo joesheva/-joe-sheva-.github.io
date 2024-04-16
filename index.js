ba194480d44b3f103e610acb61223a569d01e3a9
joesheva <159451033+joesheva@users.noreply.github.com>
2024-04-16 20:42:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -68,6 +68,33 @@ menuItems.forEach((menuItem, i) => {
     });
 });
 
+window.onscroll = function() {scrollFunction()};
+
+
+
+
+
+function scrollFunction() {
+  if (document.body.scrollTop > 105 || document.documentElement.scrollTop > 105) {
+    document.getElementById("header").style.backgroundColor = "";
+    document.getElementById("navbar").style.padding = "20px 10px";
+  
+
+    document.getElementById("text").style.color = "#0f0f10";
+    document.getElementById("text1").style.color = "#0f0f10";
+    document.getElementById("text2").style.color = "#0f0f10";
+    document.getElementById("text3").style.color = "#0f0f10";
+  } else {
+    document.getElementById("navbar").style.padding = "50px 10px";
+    document.getElementById("text").style.color = "#f6ecde";
+    document.getElementById("text1").style.color = "#f6ecde";
+    document.getElementById("text2").style.color = "#f6ecde";
+    document.getElementById("text3").style.color = "#f6ecde";
+
+    document.getElementById("header").style.backgroundColor = "#0f0f10";
+  }
+}
+
 
 const divs = document.querySelectorAll(".back-chang");
 const toolbar = document.querySelector(".toolbar");
